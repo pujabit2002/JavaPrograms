8fa466f912cdb169bb7f774eecd1328f185bac37
pujabit2002 <pujakumari11832@gmail.com>
2025-04-26 12:49:31 +0530

    add initial files

---
+++ AddTwoNumbers.java
@@ -0,0 +1,9 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int x=5;
+        int y=6;
+        int z= x+y;
+        System.out.println("The add of two numbers is:" + z);
+    }
+    
+}
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,10 @@
+/**Print "Hello, World!"
+Problem Statement: Write a program to display "Hello, World!" on the console.
+Sample Input: None
+Sample Output: Hello, World!
+Explanation: This is the simplest Java program to introduce printing output to the console. It demonstrates the basic structure of a Java program, including the main method
+**/
+
 public class HelloWorld {
     
     public static void main (String[] args){
