b893d83e801eb694fc9dd2022a94c55729ab8963
pujabit2002 <pujakumari11832@gmail.com>
2025-04-26 13:11:47 +0530

    Add Initial Files

--- CheckEvenOddNumber.java
+++ CheckEvenOddNumber.java
@@ -1,3 +1,8 @@
+/**Check Even or Odd
+Problem Statement: Write a program to check if a given number is even or odd.
+Sample Input: 4
+Sample Output: Even
+Explanation: The program uses the modulo operator (%) to determine if a number is divisible by 2. It introduces conditional statements (if-else)**/
 public class CheckEvenOddNumber {
     public static void main (String[] args){
         int x=7;
---
+++ LargestOfThreeNumbers.java
@@ -0,0 +1,21 @@
+/**Find Largest of Three Numbers
+Problem Statement: Write a program to find the largest among three input numbers.
+Sample Input: 7 12 9
+Sample Output: 12
+Explanation: The program compares three numbers using conditional statements to determine the largest. It reinforces the use of if-else or nested conditions */
+public class LargestOfThreeNumbers {
+    public static void main(String[] args){
+        int x=4;
+        int y=6;
+        int z=3;
+        if(x>y && x>z){
+            System.out.println(x);
+        }
+        else if(y>z && y>x){
+            System.out.println(y);
+        }
+        else{
+            System.out.println(z);
+        }
+    }
+}
