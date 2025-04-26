1947db09504e7a1e543de626dc15808f23ac7509
pujabit2002 <pujakumari11832@gmail.com>
2025-04-26 12:56:49 +0530

    Add Initial Files

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,3 +1,8 @@
+/**Add Two Numbers
+Problem Statement: Write a program to take two integers as input and print their sum.
+Sample Input: 5 3
+Sample Output: 8
+Explanation: The program reads two integers from the user and performs addition. It introduces input handling and basic arithmetic operations/** */
 public class AddTwoNumbers {
     public static void main(String[] args) {
         int x=5;
---
+++ CheckEvenOddNumber.java
@@ -0,0 +1,11 @@
+public class CheckEvenOddNumber {
+    public static void main (String[] args){
+        int x=7;
+        if(x%2==0){
+            System.out.println("The number is even number");
+        }
+        else{
+            System.out.println("The number is odd number");
+        }
+    }
+}
