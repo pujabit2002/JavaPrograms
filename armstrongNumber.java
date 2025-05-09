440db44d56b59b7103958c69b071fa1bfdbe8f66
pujabit2002 <pujakumari11832@gmail.com>
2025-05-09 22:20:49 +0530

    Add initial files

--- armstrongNumber.java
+++ armstrongNumber.java
@@ -1,3 +1,11 @@
+/*Armstrong Number
+Problem Statement: 
+
+Sample Input: 153
+Sample Output: Armstrong Number
+Explanation: An Armstrong number equals the sum of its digits raised to the power of the number of digits (e.g., 153 = 1³ + 5³ + 3³). The program uses loops and mathematical operations. */
 public class armstrongNumber {
-    
+    public static void main(String[] args){
+        
+    }
 }
---
+++ chekVowelOrConstant.java
@@ -0,0 +1,17 @@
+/*Check Vowel or Consonant
+Problem Statement: Write a program to check if a given character is a vowel or consonant.
+Sample Input: a
+Sample Output: Vowel
+Explanation: The program checks if the input character is one of 'a', 'e', 'i', 'o', 'u' (case-insensitive). It introduces character handling and conditionals */
+public class chekVowelOrConstant {
+    public static void main(String[] args){
+        char x='m';
+        if(x=='a' || x=='e' || x=='i' || x=='o' || x=='u'){
+            System.out.println("Vowel number");
+        }
+        else{
+            System.out.println("Constant number");
+        }
+
+    }
+}
