54391d1c88e8292403e827f4507bea3bcbfb436b
pujabit2002 <pujakumari11832@gmail.com>
2025-05-10 13:25:55 +0530

    Add initial files

--- chekVowelOrConstant.java
+++ chekVowelOrConstant.java
@@ -5,7 +5,7 @@ Sample Output: Vowel
 Explanation: The program checks if the input character is one of 'a', 'e', 'i', 'o', 'u' (case-insensitive). It introduces character handling and conditionals */
 public class chekVowelOrConstant {
     public static void main(String[] args){
-        char x='m';
+        char x='a';
         if(x=='a' || x=='e' || x=='i' || x=='o' || x=='u'){
             System.out.println("Vowel number");
         }
